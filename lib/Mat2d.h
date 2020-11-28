527f08399f0b176e587dbdeef09be8f5fa8a34cf
Lucas Gabriel <lucas.teodoro@ufu.br>
2020-11-28 09:27:10 -0300

    Tad matiz

---
+++ lib/Mat2d.h
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+typedef struct matriz matriz;
+
+int linhas(matriz *m);
+int colunas(matriz *m);
+
+matriz* allocMatriz(int i, int j);
+void freeMatriz(matriz* m);
+
+int matrizSetValue(matriz *m,int i,int j,unsigned char n);
+int matrizGetValue(matriz *m,int i,int j,unsigned char *n);
+
